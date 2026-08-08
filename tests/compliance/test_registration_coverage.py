24c7e96c3c9b9db4580e02ca95996b09541754a8
Paweł <pawel.maciejewski95@gmail.com>
2026-08-08 03:40:59 +0200

    test: cover public resource registration inventory

--- tests/compliance/test_registration_coverage.py
+++ tests/compliance/test_registration_coverage.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 import pytest
 
-from tests.integration.conftest import RegistryMCP
+from local_home_devices_mcp.manifests import (
+    ARTIFACT_READ_MANIFEST,
+    PUBLIC_RESOURCE_COMPONENTS,
+    normalize_catalog,
+)
 from tools.constants import TOOL_MANIFESTS
 from tools.iot_config import register_iot_config_tools
 from tools.iot_control import register_iot_control_tools
@@ -15,6 +19,25 @@ from tools.iot_openhasp import register_openhasp_tools
 from tools.iot_tuya import register_iot_tuya_tools
 
 pytestmark = pytest.mark.unit
+
+
+class RegistryMCP:
+    """Minimal decorator registry used for zero-I/O registration coverage."""
+
+    def __init__(self) -> None:
+        self.tools: dict[str, object] = {}
+
+    def tool(self, *args, **kwargs):
+        if args and callable(args[0]) and len(args) == 1 and not kwargs:
+            function = args[0]
+            self.tools[function.__name__] = function
+            return function
+
+        def decorator(function):
+            self.tools[function.__name__] = function
+            return function
+
+        return decorator
 
 
 def test_every_registered_tool_has_manifest_and_no_unexplained_orphans():
@@ -42,3 +65,18 @@ def test_every_registered_tool_has_manifest_and_no_unexplained_orphans():
         "hikvision_pipeline_diagnose",
         "hikvision_restart_container",
     }
+
+
+def test_public_resource_inventory_has_one_manifest_per_component():
+    catalog = normalize_catalog(TOOL_MANIFESTS)
+    catalog[ARTIFACT_READ_MANIFEST["id"]] = dict(ARTIFACT_READ_MANIFEST)
+
+    assert PUBLIC_RESOURCE_COMPONENTS == {
+        "artifact://{artifact_id}": "artifact_read",
+    }
+    resource_manifest_ids = set(PUBLIC_RESOURCE_COMPONENTS.values())
+    assert resource_manifest_ids <= set(catalog)
+    assert {
+        catalog[item]["extensions"]["component_kind"]
+        for item in resource_manifest_ids
+    } == {"resource"}
